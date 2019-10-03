8593de3527898a449e3954d2d0d216f9a8ac2e0f
mjanssen <dev.marnix@gmail.com>
2019-10-03 20:32:23 +0200

    Moved styling to app.css

--- src/App.js
+++ src/App.js
@@ -3,18 +3,9 @@ import './App.css';
 import { TopMenu, Footer } from './Components/Layouts';
 import Maincontent from './Components/Maincontent';
 
-var sectionStyle = {
-  backgroundImage: 'url(https://tj-static.s3.ap-south-1.amazonaws.com/etc/Capture.png)',
-  backgroundRepeat: 'repeat',
-  backgroundPositionX: 'center',
-  position: 'absolute',
-  width: '100%',
-  top: '0',
-};
-
 function App() {
   return (
-    <div className="App" style={sectionStyle}>
+    <div className="App sectionStyle">
       <Fragment>
         <TopMenu />
         <Maincontent />
